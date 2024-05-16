29e9accb0c5f3b1a277074e5602476e6aba07489
Marcellofabrizio <marcfabrizio10@gmail.com>
2024-05-15 22:41:11 -0300

    revert db

--- internal/server/server.go
+++ internal/server/server.go
@@ -20,11 +20,10 @@ type Server struct {
 
 func NewServer() *http.Server {
 	port, _ := strconv.Atoi(os.Getenv("PORT"))
-	db := database.New()
 	NewServer := &Server{
 		port: port,
 
-		db: db,
+		db: database.New(),
 	}
 
 	// Declare Server config
